ee90b94d6357950abb0c18e85323c79fa00f7558
Samuel Heer <samuel@ex-akt.de>
2025-01-27 10:45:00 +0100

    add language de

---
+++ contao/languages/de/tl_thememanager.php
@@ -0,0 +1,6 @@
+<?php 
+
+declare(strict_types=1);
+$GLOBALS['TL_LANG']['tl_thememanager']['footer'] = "Erweiterung: Footer";
+$GLOBALS['TL_LANG']['tl_thememanager']['f-mt'] = array("\$f-mt","Geben Sie hier den oberen äußeren Abstand des Footers an.");
+$GLOBALS['TL_LANG']['tl_thememanager']['f-background'] = array("\$f-background","Vergebe Sie hier die Hintergrundfarbe des Footers.");
